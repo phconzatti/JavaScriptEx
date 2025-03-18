f61cebc4f5c9b121847ca6657beebf1d94c435ed
phconzatti <phconzatti@hotmail.com>
2025-03-18 11:38:58 -0300

    Valor padrão

---
+++ ValorPadrao.js
@@ -0,0 +1,5 @@
+function soma (a=1, b=2, c=3){
+    return a+b+c;
+}
+
+console.log(soma(), soma(2));
--- teste.js
+++ teste.js
@@ -13,3 +13,16 @@ console.log(numeroDouble);
 
 let texto = "jajajajajja";
 console.log(texto);
+
+const nota = 7.5
+
+if (nota>=5) {
+    console.log('Aprovado')
+} else {
+    console.log('Reprovado')
+}
+
+let num1 = 1
+let num2 ='1'
+
+if (num1 === num2) console.log("igual")    
